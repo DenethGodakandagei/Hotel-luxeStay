ee6afde609142b703610ca74f4d3431ff232fee8
Deneth  Godakandagei <138301396+DenethGodakandagei@users.noreply.github.com>
2023-07-20 22:45:11 +0530

    Update RoomReview4.jsx

--- src/pages/RoomsReview/RoomReview4.jsx
+++ src/pages/RoomsReview/RoomReview4.jsx
@@ -30,18 +30,20 @@ export const RoomReview4 = () => {
       >
         {(close) => (
           <div className="modal overlay">
-            <div className="popupContent">
-              <header>
-                <div className="flex justify-end">
+            <div className="m-[30px] sm:m-10px">
+            <header>
+                <div className="flex justify-between">
+                  <div className="flex">
                   <img src={logo} style={{ width: "70px" }} />
                   <span className="mt-6 text-orange-400">LuxeStay</span>
-                  <div className="flex justify-end">
+                  </div>
+                  <div className="flex justify-between">
                     <button
-                      className="m-3 bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      className="bg-red-600 hover:bg-red-500 text-white font-bold  rounded focus:outline-none focus:shadow-outline w-[18x] h-[18px] mt-5"
                       onClick={() => close()}
                     >
                       <svg
-                        class="h-4 w-4  "
+                        class="h-5 w-5 "
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
@@ -62,28 +64,28 @@ export const RoomReview4 = () => {
               </header>
               <div className="container mx-auto">
                 <div class="popup   flex justify-center grid lg:grid-cols-2 gap-6">
-                  <div className="max-w-lg ">
+                  <div className="max-w-[500px] p-1 m-2 flex justify-center">
                     <img
                       class="object-cover w-full rounded m-5  "
                       src={Room4}
                       alt="/"
                     />
                   </div>
-                  <div className="p-3 m-3">
+                  <div className="p-1">
                     <h5 class=" text-2xl font-bold leading-none  justify-center my-3 ">
                       Double Room
                     </h5>
                     <span class="  leading-none  text-orange-400">
                     Our Double Rooms are perfect for guests seeking comfortable yet affordable accommodations.
                     </span>
-                    <ul className="list-disc p-5">
-                      <li className="p-3">
+                    <ul className="list-disc p-5 text-justify ">
+                      <li className="p-1 sm:p-3">
                       Offering a cozy and inviting space, these rooms feature twin beds or a spacious double bed, guaranteeing a restful night's sleep.
                       </li >
-                      <li className="p-3">
+                      <li className="p-1 sm:p-3">
                       Take advantage of the amenities available, including a TV, a work desk, and complimentary Wi-Fi. 
                       </li>
-                      <li className="p-3">
+                      <li className="p-1 sm:p-3">
                       The thoughtfully designed interiors provide a tranquil ambiance, allowing guests to relax and unwind after a long day of exploring.
                       </li>
                      
